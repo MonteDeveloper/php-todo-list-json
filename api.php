669639559db3a9d82df594bacb364d15a577edb5
MonteDeveloper <lorenzomontello83@gmail.com>
2023-06-13 16:24:45 +0200

    move data to JSON file

--- api.php
+++ api.php
@@ -1,51 +1,8 @@
 <?php
 
-$toDoList = [
-    [
-        "text" => "Fare la spesa",
-        "did" => true
-    ],
-    [
-        "text" => "Pagare le bollette",
-        "did" => false
-    ],
-    [
-        "text" => "Andare in palestra",
-        "did" => true
-    ],
-    [
-        "text" => "Chiamare mamma",
-        "did" => false
-    ],
-    [
-        "text" => "Prenotare il viaggio",
-        "did" => true
-    ],
-    [
-        "text" => "Comprare i biglietti del cinema",
-        "did" => false
-    ],
-    [
-        "text" => "Preparare la cena",
-        "did" => true
-    ],
-    [
-        "text" => "Lavare i piatti",
-        "did" => false
-    ],
-    [
-        "text" => "Portare fuori il cane",
-        "did" => true
-    ],
-    [
-        "text" => "Riordinare la stanza",
-        "did" => false
-    ]
-];
-
-$toDoListJson = json_encode($toDoList);
-
 header('Content-Type: application/json');
+
+$toDoListJson = file_get_contents('database.json');
 
 echo $toDoListJson;
 
